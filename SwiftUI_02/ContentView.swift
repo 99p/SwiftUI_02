bde064789c59e094669b2c1a226631b2f00f7bb2
99p <poccori@gmail.com>
2021-10-16 04:21:03 +0900

    Initial Commit

---
+++ SwiftUI_02/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_02
+//
+//  Created by macboy on 2021/10/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
