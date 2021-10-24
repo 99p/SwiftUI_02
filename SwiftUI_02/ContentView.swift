791ca9b019a69c99232b7ec7edb86e4674b6df46
99p <poccori@gmail.com>
2021-10-24 22:02:01 +0900

    cool!

--- SwiftUI_02/ContentView.swift
+++ SwiftUI_02/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                .aspectRatio(contentMode: .fill)
+        }
     }
 }
 
